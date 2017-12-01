4e4415e11b68e7b523951e8c7fdaea5624a5fa42
Daniel Molina <daniel.97.molin@gmail.com>
2017-12-01 08:52:11 -0600

    cuadro modal

--- src/main/java/com/example/demo/controllers/ApiController.java
+++ src/main/java/com/example/demo/controllers/ApiController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.demo.models.Miembro;
 import com.example.demo.models.MiembroTabla;
+import com.example.demo.models.Pago;
 import com.example.demo.service.RestFulService;
 
 @Controller
@@ -19,36 +20,39 @@ public class ApiController {
 
 	@Autowired
 	RestFulService rest;
-	
+
 	@GetMapping("/")
-	public String get(ModelMap model,@CookieValue(value="empleado",required=false) String cookie) {
-//model.addAttribute("lista",rest.getAllEmpleados());
-		return render(cookie,model,1);
+	public String get(ModelMap model, @CookieValue(value = "empleado", required = false) String cookie) {
+		// model.addAttribute("lista",rest.getAllEmpleados());
+		//return render(cookie, model, 1);
+		return "redirect:/1";
 	}
-	
+
 	@GetMapping("/{idSucursal}")
-	public String gett(ModelMap model,@CookieValue(value="empleado",required=false) String cookie,@PathVariable int idSucursal) {
-//model.addAttribute("lista",rest.getAllEmpleados());
-		return render(cookie,model,idSucursal);
+	public String gett(ModelMap model, @CookieValue(value = "empleado", required = false) String cookie,
+			@PathVariable int idSucursal) {
+		// model.addAttribute("lista",rest.getAllEmpleados());
+		return render(cookie, model, idSucursal);
 	}
 
-
-private String render(String cookie,ModelMap model,int idSucursal) {
-	//java.util.Date fecha=new java.util.Date(); 
-	
-	if(cookie==null) {
-		return "index";
-	}
-	else {
-		List<MiembroTabla> lista=new ArrayList<MiembroTabla>();
-		for(Miembro m:rest.getMiembros(idSucursal)) {
-			lista.add(new MiembroTabla("todo",rest.ultimoPago(m.getId()).getFecha(),m.getId(),m.getNombre(),m.isMasculino(),m.isActivo(),
-					m.getSucursal_id(), m.getFechaingreso()));
+	private String render(String cookie, ModelMap model, int idSucursal) {
+		java.sql.Date fecha = new java.sql.Date(new java.util.Date().getTime()-(1000*60*60*60*24*30));
+		
+		if (cookie == null) {
+			return "index";
+		} else {
+			List<MiembroTabla> lista = new ArrayList<MiembroTabla>();
+			for (Miembro m : rest.getMiembros(idSucursal)) {
+				Pago ultimo = rest.ultimoPago(m.getId());
+				lista.add(new MiembroTabla( (fecha.after(ultimo.getFecha()))?"Atrasado":"Al día" , 
+						ultimo.getFecha(), m.getId(), m.getNombre(),
+						m.isMasculino(), m.isActivo(), m.getSucursal_id(), m.getFechaingreso()));
+			}
+			model.addAttribute("empleado", rest.getEmpleado(Integer.parseInt(cookie)));
+			model.addAttribute("sucursales", rest.getSucursals());
+			model.addAttribute("miembros", lista);
+			model.addAttribute("idSucursal", "5");
+			return "miembros";
 		}
-	model.addAttribute("empleado",rest.getEmpleado(Integer.parseInt(cookie)));
-	model.addAttribute("sucursales",rest.getSucursals());	
-	model.addAttribute("miembros",lista);
-	return "miembros";
 	}
-}
 }
--- src/main/resources/static/javascript/miembros.js
+++ src/main/resources/static/javascript/miembros.js
@@ -1,7 +1,18 @@
 $(document).ready(function() {
-	$("#cmbSucursal").val('3');
+	
 });
 
 $("#cmbSucursal").change(function() {
-location.href=this.value;	
+	location.href=this.value;	
 });
+
+$("#btnCerrarSesion").click(function(){
+	document.cookie="empleado=;expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+	location.href="/";
+});
+
+
+
+
+
+
