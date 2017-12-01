48f6c843f74deb9fc80322f11e03bbd7f50377cd
Christian Herrera <herreraChristian1897@gmail.com>
2017-12-01 05:06:17 -0600

    cambio de sucursal

--- src/main/java/com/example/demo/controllers/ApiController.java
+++ src/main/java/com/example/demo/controllers/ApiController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.demo.models.Miembro;
 import com.example.demo.models.MiembroTabla;
@@ -22,19 +23,32 @@ public class ApiController {
 	@GetMapping("/")
 	public String get(ModelMap model,@CookieValue(value="empleado",required=false) String cookie) {
 //model.addAttribute("lista",rest.getAllEmpleados());
-		if(cookie==null) {
-			return "index";
-		}
-		else {
-			List<MiembroTabla> lista=new ArrayList<MiembroTabla>();
-			for(Miembro m:rest.getMiembros(1)) {
-				lista.add(new MiembroTabla("todo",rest.ultimoPago(m.getId()).getFecha(),m.getId(),m.getNombre(),m.isMasculino(),m.isActivo(),
-						m.getSucursal_id(), m.getFechaingreso()));
-			}
-		model.addAttribute("empleado",rest.getEmpleado(Integer.parseInt(cookie)));
-		model.addAttribute("sucursales",rest.getSucursals());	
-		model.addAttribute("miembros",lista);
-		return "miembros";
-		}
+		return render(cookie,model,1);
 	}
+	
+	@GetMapping("/{idSucursal}")
+	public String gett(ModelMap model,@CookieValue(value="empleado",required=false) String cookie,@PathVariable int idSucursal) {
+//model.addAttribute("lista",rest.getAllEmpleados());
+		return render(cookie,model,idSucursal);
+	}
+
+
+private String render(String cookie,ModelMap model,int idSucursal) {
+	//java.util.Date fecha=new java.util.Date(); 
+	
+	if(cookie==null) {
+		return "index";
+	}
+	else {
+		List<MiembroTabla> lista=new ArrayList<MiembroTabla>();
+		for(Miembro m:rest.getMiembros(idSucursal)) {
+			lista.add(new MiembroTabla("todo",rest.ultimoPago(m.getId()).getFecha(),m.getId(),m.getNombre(),m.isMasculino(),m.isActivo(),
+					m.getSucursal_id(), m.getFechaingreso()));
+		}
+	model.addAttribute("empleado",rest.getEmpleado(Integer.parseInt(cookie)));
+	model.addAttribute("sucursales",rest.getSucursals());	
+	model.addAttribute("miembros",lista);
+	return "miembros";
+	}
+}
 }
--- src/main/resources/static/javascript/miembros.js
+++ src/main/resources/static/javascript/miembros.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
 	$("#cmbSucursal").val('3');
 });
+
+$("#cmbSucursal").change(function() {
+location.href=this.value;	
+});
