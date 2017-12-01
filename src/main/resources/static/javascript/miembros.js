5542de0bb98793163422edb76612f2f4805c00a2
Christian Herrera <herreraChristian1897@gmail.com>
2017-12-01 11:44:00 -0600

    creacion de miembros y pagos

--- src/main/resources/static/javascript/miembros.js
+++ src/main/resources/static/javascript/miembros.js
@@ -11,8 +11,40 @@ $("#btnCerrarSesion").click(function(){
 	location.href="/";
 });
 
+function x() {
+	var fecha=new Date();
+	alert(fecha.toISOString());
+}
+$(".agregarMiembro").submit(function() {
+	var idSucursal=location.href.split("/")[3];
+	var fecha=new Date();
+	var fechaConFormato=fecha.toISOString().split("T")[0];
+	$.ajax({
+		  type: "POST",
+		  url: "https://gym-danm97.c9users.io/sucursals/"+idSucursal+"/miembros",
+		  data: {'nombre':this.nombre.value,'masculino':this.radio.value=="masc"?true:false,'activo':true,
+				  'fechaingreso':fechaConFormato},
+		  success: function(r){
+			  
+				  realizarPago(idSucursal,fechaConFormato,r.id);
+		 },
+		  dataType: "json"
+		});
+});
 
-
-
-
+function realizarPago(idSucursal,fecha,id){
+$.ajax({
+	  type: "POST",
+	  url: "https://gym-danm97.c9users.io/sucursals/"+idSucursal+"/miembros/"+id+"/pagos",
+	  data: {
+	        fecha: fecha,
+	        empleado_id: document.cookie.split("empleado=")[1],
+	        tipopago_id: 1
+	},
+	  success: function(r){
+		  location.reload();
+	  },
+	  dataType: "json"
+	});
+}
 
